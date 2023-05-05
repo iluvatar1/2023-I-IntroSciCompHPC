60ddb1bc0d2dd3a3d5a6971faef5a8f10ac32f5d
iluvatar1 <woquendo@gmail.com>
2023-05-05 12:39:02 -0500

    Added mutex example

---
+++ 2023-05-05-threads-parallel-stl/threads_exercise.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+
+void square(const int x, int & result);
+
+int main() {
+    int accum = 0;
+    std::vector<std::thread> ths;
+    for (int i = 1; i <= 20; i++) {
+        ths.push_back(std::thread(&square, i, std::ref(accum)));
+    }
+
+    for (auto & th : ths) {
+        th.join();
+    }
+    std::cout << "accum = " << accum << std::endl;
+    return 0;
+}
+
+void square(int x, int &result) {
+    result += x * x;
+}
