1a922c244e5731d6cac7f8ada34880b98309959e
iluvatar1 <woquendo@gmail.com>
2023-05-11 09:34:25 -0500

    Added cuda examples

---
+++ 2023-05-10-IntroGPU-Cuda/example_02.cu
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define N 10000000
+
+__global__ void vector_add(float *out, float *a, float *b, int n) {
+    for(int i = 0; i < n; i++){
+        out[i] = a[i] + b[i];
+    }
+}
+
+int main(){
+    float *a, *b, *out;
+    float *d_a, *d_b, *d_out;
+
+    // Allocate host memory
+    a   = (float*)malloc(sizeof(float) * N);
+    b   = (float*)malloc(sizeof(float) * N);
+    out = (float*)malloc(sizeof(float) * N);
+
+    // Initialize array
+    for(int i = 0; i < N; i++){
+        a[i] = 1.0f;
+	b[i] = 2.0f;
+    }
+
+    // Allocate device memory
+    cudaMalloc((void**)&d_a, sizeof(float) * N);
+    cudaMalloc((void**)&d_b, sizeof(float) * N);
+    cudaMalloc((void**)&d_out, sizeof(float) * N);
+
+    // Transfer data from host to device memory
+    cudaMemcpy(d_a, a, sizeof(float) * N, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b, sizeof(float) * N, cudaMemcpyHostToDevice);
+
+    // Main function
+    vector_add<<<1,256>>>(out, a, b, N);
+
+   // Transfer data back to host memory
+    cudaMemcpy(out, d_out, sizeof(float) * N, cudaMemcpyDeviceToHost);
+
+    // print some result
+    printf("%g\n", out[0]);
+
+    // Deallocate device memory
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_out);
+
+    // Deallocate host memory
+    free(a);
+    free(b);
+    free(out);
+
+    return 0;
+}
